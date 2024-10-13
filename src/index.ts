a274dd574ffee19e262f7eb456c79feb24aface3
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-13 06:06:27 +0200

    feat: add `getValidatedInput` helper

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export function hello() {
-  return "Hello, world!";
+import type { ZodType } from "zod";
+import type { InputOptions } from "@actions/core";
+import { getInput } from "@actions/core";
+
+export function getValidatedInput<TSchema extends ZodType>(inputName: string, schema: TSchema, options?: InputOptions): TSchema["_output"] {
+  const raw = getInput(inputName, options);
+
+  return schema.safeParse(raw);
 }
