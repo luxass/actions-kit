fca49ae31f10ebe9eaaa1cd8c2462f1fbcee7e72
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-13 06:32:11 +0200

    feat: add better types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import type { InputOptions } from "@actions/core";
-import type { ZodType } from "zod";
+import type { SafeParseReturnType, ZodType } from "zod";
 import { getInput } from "@actions/core";
 
-export function getValidatedInput<TSchema extends ZodType>(inputName: string, schema: TSchema, options?: InputOptions): TSchema["_output"] {
+/**
+ * Retrieves an input value by its name, validates it against a provided Zod schema, and returns the result of the validation.
+ *
+ * @template TSchema - The Zod schema type used for validation.
+ * @param {string} inputName - The name of the input to retrieve.
+ * @param {TSchema} schema - The Zod schema to validate the input against.
+ * @param {InputOptions} [options] - Optional settings for retrieving the input.
+ * @returns {SafeParseReturnType<unknown, TSchema["_output"]>} The result of the validation, containing either the parsed value or validation errors.
+ */
+export function getValidatedInput<TSchema extends ZodType>(inputName: string, schema: TSchema, options?: InputOptions): SafeParseReturnType<unknown, TSchema["_output"]> {
   const raw = getInput(inputName, options);
 
   return schema.safeParse(raw);
