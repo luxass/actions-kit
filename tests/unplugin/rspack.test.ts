25d5f25ba32832cb27820b39fcbd6b2f1ab2a94b
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-17 10:55:20 +0200

    test: write tests for rspack

--- tests/unplugin/rspack.test.ts
+++ tests/unplugin/rspack.test.ts
@@ -1,8 +1,7 @@
 import { readdir, readFile } from "node:fs/promises";
 import path, { join } from "node:path";
 import { type Configuration, rspack as createRspack, type Stats } from "@rspack/core";
-import { build } from "esbuild";
-import { expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { fromFileSystem, testdir } from "vitest-testdirs";
 import ActionKitPlugin from "../../src/unplugin/rspack";
 
@@ -121,8 +120,297 @@ it("expect `actions-kit.d.ts` to be generated", async () => {
   expect(dtsOutput).toMatchSnapshot();
 });
 
-it("inject inputs", async () => {
-  const directoryJson = await fromFileSystem("./tests/fixtures/without-globals");
+describe("augmentations", () => {
+  it("expect only inputs to be augmented", async () => {
+    const directoryJson = await fromFileSystem("./tests/fixtures/only-inputs");
+    const testdirPath = await testdir(directoryJson);
+
+    expect(testdirPath).toBeDefined();
+
+    const result = await rspack(testdirPath, {
+      entry: join(testdirPath, "index.ts"),
+      plugins: [
+        ActionKitPlugin({
+          actionPath: join(testdirPath, "action.yaml"),
+        }),
+      ],
+    });
+
+    const json = result?.toJson();
+    expect(json).toBeDefined();
+
+    expect(json?.errors).toBeDefined();
+
+    if ((json?.errors || []).length > 0) {
+      console.error(json?.errors);
+    }
+
+    expect(json?.errors).toHaveLength(0);
+
+    const file = json!.assetsByChunkName!.main;
+    const content = await readFile(path.join(testdirPath, file![0]!), "utf-8");
+
+    expect(content).toMatchSnapshot();
+
+    const dtsOutput = await readFile(join(testdirPath, "actions-kit.d.ts"), "utf-8");
+
+    expect(dtsOutput).not.toContain("ActionOutputName");
+    expect(dtsOutput).toContain("ActionInputName");
+
+    expect(dtsOutput).toMatchSnapshot();
+  });
+
+  it("expect only outputs to be augmented", async () => {
+    const directoryJson = await fromFileSystem("./tests/fixtures/only-outputs");
+    const testdirPath = await testdir(directoryJson);
+
+    expect(testdirPath).toBeDefined();
+
+    const result = await rspack(testdirPath, {
+      entry: join(testdirPath, "index.ts"),
+      plugins: [
+        ActionKitPlugin({
+          actionPath: join(testdirPath, "action.yaml"),
+        }),
+      ],
+    });
+
+    const json = result?.toJson();
+    expect(json).toBeDefined();
+
+    expect(json?.errors).toBeDefined();
+
+    if ((json?.errors || []).length > 0) {
+      console.error(json?.errors);
+    }
+
+    expect(json?.errors).toHaveLength(0);
+
+    const file = json!.assetsByChunkName!.main;
+    const content = await readFile(path.join(testdirPath, file![0]!), "utf-8");
+
+    expect(content).toMatchSnapshot();
+
+    const dtsOutput = await readFile(join(testdirPath, "actions-kit.d.ts"), "utf-8");
+
+    expect(dtsOutput).toContain("ActionOutputName");
+    expect(dtsOutput).not.toContain("ActionInputName");
+
+    expect(dtsOutput).toMatchSnapshot();
+  });
+
+  it("expect no augmentations", async () => {
+    const directoryJson = await fromFileSystem("./tests/fixtures/empty");
+    const testdirPath = await testdir(directoryJson);
+
+    expect(testdirPath).toBeDefined();
+
+    const result = await rspack(testdirPath, {
+      entry: join(testdirPath, "index.ts"),
+      plugins: [
+        ActionKitPlugin({
+          actionPath: join(testdirPath, "action.yaml"),
+        }),
+      ],
+    });
+
+    const json = result?.toJson();
+    expect(json).toBeDefined();
+
+    expect(json?.errors).toBeDefined();
+
+    if ((json?.errors || []).length > 0) {
+      console.error(json?.errors);
+    }
+
+    expect(json?.errors).toHaveLength(0);
+
+    const file = json!.assetsByChunkName!.main;
+    const content = await readFile(path.join(testdirPath, file![0]!), "utf-8");
+
+    expect(content).toMatchSnapshot();
+
+    const dtsOutput = await readFile(join(testdirPath, "actions-kit.d.ts"), "utf-8");
+
+    expect(dtsOutput).not.toContain("ActionOutputName");
+    expect(dtsOutput).not.toContain("ActionInputName");
+
+    expect(dtsOutput).toMatchSnapshot();
+  });
+});
+
+describe("inject", () => {
+  it("expect `ACTION_INPUTS` in global scope", async () => {
+    const directoryJson = await fromFileSystem("./tests/fixtures/only-inputs");
+    const testdirPath = await testdir(directoryJson);
+
+    expect(testdirPath).toBeDefined();
+
+    const result = await rspack(testdirPath, {
+      entry: join(testdirPath, "index.ts"),
+      plugins: [
+        ActionKitPlugin({
+          actionPath: join(testdirPath, "action.yaml"),
+          inject: "inputs",
+        }),
+      ],
+    });
+
+    const json = result?.toJson();
+    expect(json).toBeDefined();
+
+    expect(json?.errors).toBeDefined();
+
+    if ((json?.errors || []).length > 0) {
+      console.error(json?.errors);
+    }
+
+    expect(json?.errors).toHaveLength(0);
+
+    const file = json!.assetsByChunkName!.main;
+    const content = await readFile(path.join(testdirPath, file![0]!), "utf-8");
+
+    expect(content).toMatchSnapshot();
+
+    const dtsOutput = await readFile(join(testdirPath, "actions-kit.d.ts"), "utf-8");
+
+    expect(dtsOutput).not.toContain("ActionOutputName");
+    expect(dtsOutput).not.toContain("ACTION_OUTPUTS");
+    expect(dtsOutput).toContain("ActionInputName");
+    expect(dtsOutput).toContain("ACTION_INPUTS");
+
+    expect(dtsOutput).toMatchSnapshot();
+  });
+
+  it("expect `ACTION_OUTPUTS` in global scope", async () => {
+    const directoryJson = await fromFileSystem("./tests/fixtures/only-outputs");
+    const testdirPath = await testdir(directoryJson);
+
+    expect(testdirPath).toBeDefined();
+
+    const result = await rspack(testdirPath, {
+      entry: join(testdirPath, "index.ts"),
+      plugins: [
+        ActionKitPlugin({
+          actionPath: join(testdirPath, "action.yaml"),
+          inject: "outputs",
+        }),
+      ],
+    });
+
+    const json = result?.toJson();
+    expect(json).toBeDefined();
+
+    expect(json?.errors).toBeDefined();
+
+    if ((json?.errors || []).length > 0) {
+      console.error(json?.errors);
+    }
+
+    expect(json?.errors).toHaveLength(0);
+
+    const file = json!.assetsByChunkName!.main;
+    const content = await readFile(path.join(testdirPath, file![0]!), "utf-8");
+
+    expect(content).toMatchSnapshot();
+
+    const dtsOutput = await readFile(join(testdirPath, "actions-kit.d.ts"), "utf-8");
+
+    expect(dtsOutput).toContain("ActionOutputName");
+    expect(dtsOutput).toContain("ACTION_OUTPUTS");
+    expect(dtsOutput).not.toContain("ActionInputName");
+    expect(dtsOutput).not.toContain("ACTION_INPUTS");
+
+    expect(dtsOutput).toMatchSnapshot();
+  });
+
+  it("expect `ACTION_INPUTS` & `ACTION_OUTPUTS` in global scope", async () => {
+    const directoryJson = await fromFileSystem("./tests/fixtures/basic");
+    const testdirPath = await testdir(directoryJson);
+
+    expect(testdirPath).toBeDefined();
+
+    const result = await rspack(testdirPath, {
+      entry: join(testdirPath, "index.ts"),
+      plugins: [
+        ActionKitPlugin({
+          actionPath: join(testdirPath, "action.yaml"),
+          inject: true,
+        }),
+      ],
+    });
+
+    const json = result?.toJson();
+    expect(json).toBeDefined();
+
+    expect(json?.errors).toBeDefined();
+
+    if ((json?.errors || []).length > 0) {
+      console.error(json?.errors);
+    }
+
+    expect(json?.errors).toHaveLength(0);
+
+    const file = json!.assetsByChunkName!.main;
+    const content = await readFile(path.join(testdirPath, file![0]!), "utf-8");
+
+    expect(content).toMatchSnapshot();
+
+    const dtsOutput = await readFile(join(testdirPath, "actions-kit.d.ts"), "utf-8");
+
+    expect(dtsOutput).toContain("ActionOutputName");
+    expect(dtsOutput).toContain("ACTION_OUTPUTS");
+    expect(dtsOutput).toContain("ActionInputName");
+    expect(dtsOutput).toContain("ACTION_INPUTS");
+
+    expect(dtsOutput).toMatchSnapshot();
+  });
+
+  it("expect no change in global scope", async () => {
+    const directoryJson = await fromFileSystem("./tests/fixtures/basic");
+    const testdirPath = await testdir(directoryJson);
+
+    expect(testdirPath).toBeDefined();
+
+    const result = await rspack(testdirPath, {
+      entry: join(testdirPath, "index.ts"),
+      plugins: [
+        ActionKitPlugin({
+          actionPath: join(testdirPath, "action.yaml"),
+          inject: false,
+        }),
+      ],
+    });
+
+    const json = result?.toJson();
+    expect(json).toBeDefined();
+
+    expect(json?.errors).toBeDefined();
+
+    if ((json?.errors || []).length > 0) {
+      console.error(json?.errors);
+    }
+
+    expect(json?.errors).toHaveLength(0);
+
+    const file = json!.assetsByChunkName!.main;
+    const content = await readFile(path.join(testdirPath, file![0]!), "utf-8");
+
+    expect(content).toMatchSnapshot();
+
+    const dtsOutput = await readFile(join(testdirPath, "actions-kit.d.ts"), "utf-8");
+
+    expect(dtsOutput).toContain("ActionOutputName");
+    expect(dtsOutput).not.toContain("ACTION_OUTPUTS");
+    expect(dtsOutput).toContain("ActionInputName");
+    expect(dtsOutput).not.toContain("ACTION_INPUTS");
+
+    expect(dtsOutput).toMatchSnapshot();
+  });
+});
+
+it("custom output path", async () => {
+  const directoryJson = await fromFileSystem("./tests/fixtures/basic");
   const testdirPath = await testdir(directoryJson);
 
   expect(testdirPath).toBeDefined();
@@ -132,7 +420,7 @@ it("inject inputs", async () => {
     plugins: [
       ActionKitPlugin({
         actionPath: join(testdirPath, "action.yaml"),
-        injectInputs: true,
+        outputPath: join(testdirPath, "custom"),
       }),
     ],
   });
@@ -151,11 +439,9 @@ it("inject inputs", async () => {
   const file = json!.assetsByChunkName!.main;
   const content = await readFile(path.join(testdirPath, file![0]!), "utf-8");
 
-  const dtsOutput = await readFile(join(testdirPath, "actions-kit.d.ts"), "utf-8");
-
-  expect(dtsOutput).toMatchSnapshot();
   expect(content).toMatchSnapshot();
 
-  expect(content).toContain("ACTION_INPUTS");
-  expect(dtsOutput).toContain("ACTION_INPUTS");
+  const dtsOutput = await readFile(join(testdirPath, "custom", "actions-kit.d.ts"), "utf-8");
+
+  expect(dtsOutput).toMatchSnapshot();
 });
