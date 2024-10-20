3e2d3fe5fe7bb14bdaf915c7d5e0ab8562a2a0d0
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-20 13:05:38 +0200

    dump

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,11 +1,7 @@
 import core from "@actions/core";
-import { defineAction } from "actions-kit"
+import { defineAction } from "actions-kit";
 
-defineAction(({
-  inputs,
-  outputs
-}) => {
-  // biome-ignore lint/style/noNonNullAssertion: <explanation>
-  const name = core.getInput(inputs.name!);
-
-})
+defineAction(({ inputs, outputs }) => {
+	// biome-ignore lint/style/noNonNullAssertion: <explanation>
+	const name = core.getInput(inputs.name!);
+});
