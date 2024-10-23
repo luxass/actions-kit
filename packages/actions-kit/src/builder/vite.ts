a79165ffa7de7557d84c8593658286ed89f0cbe6
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-23 19:54:00 +0200

    feat: add initial vite support

--- packages/actions-kit/src/builder/vite.ts
+++ packages/actions-kit/src/builder/vite.ts
@@ -1,6 +1,47 @@
+import { inferModuleType, inferOutputFilename } from "../utils";
 import type { Config } from "../config";
-import type { UserConfig } from "vite";
+import { build as viteBuild } from "vite";
 
-export async function build(config?: Config) {
-	throw new Error("not implemented yet.");
+
+export async function build(config: Config) {
+	const outputFileName = await inferOutputFilename(config);
+	const libraryType = await inferModuleType(config, outputFileName);
+
+	const viteConfig = config.vite;
+
+	const viteOptions = {
+		mode: "production",
+		...viteConfig,
+		build: {
+			outDir: "dist",
+			rollupOptions: {
+				output: {
+					fileName: outputFileName,
+					format: libraryType,
+				},
+			},
+		},
+	};
+
+	const result = await viteBuild({
+		build: {
+			minify: false,
+			target: "node20",
+			ssr: true,
+			rollupOptions: {
+				input: ["src/index.ts"],
+				output: {
+					entryFileNames: outputFileName,
+					format: libraryType,
+					exports: "auto",
+				},
+			},
+		},
+		ssr: {
+			// Anything NOT 'node:' will be bundled.
+			noExternal: /^(?!node:)/,
+		},
+	})
+
+	console.log(result)
 }
