6a8ffb13c240c249dc05437ea35c4c06ec016a88
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-27 11:48:44 +0100

    fix(builders): publish cjs

--- packages/builders/esbuild/tsup.config.ts
+++ packages/builders/esbuild/tsup.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["src/**/*.ts", "index.d.ts"],
-	format: ["esm"],
+	entry: ["src/index.ts"],
+	format: ["esm", "cjs"],
 	dts: true,
 	splitting: true,
 	clean: true,
